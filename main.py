bcdc40ea864aa99ae5e6de7afa53ec4af6284e10
Jamie McNally <17f23206@marling.school>
2020-09-16 10:10:49 +0000

    swamp

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ noun = "fridge"
 adverb = "magestically"
 adjective = "dead"
 print(f"The {noun} walked {adjective} when it {adverb} robbed the shoe shop")
-swamp = input("what are you doing in my SWAMP")
-name = input("WHAT IS UR NAME")
+swamp = input("what are you doing in my SWAMP!! ")
+name = input("WHAT IS UR NAME!? ")
 
-noun = "fridge"
-adverb = "aggressively"
-adjective = "giant"
-print(f"he was {adjective} when it {adverb} went to school")
+print(f"WELL THEN {name} how would you like to GET OUT! yes or no")
+
