e8c7bc6def86123870ec550c58684fb3e8dc3692
Jamie McNally <17f23206@marling.school>
2020-09-16 09:25:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+noun = "fridge"
+adverb = "magestically"
+adjective = "dead"
+print(f"The {noun} walked {adjective} when it {adverb} robbed the shoe shop")
+swamp = input("what are you doing in my SWAMP")
+name = input("WHAT IS UR NAME")
+
+noun = "fridge"
+adverb = "aggressively"
+adjective = "giant"
+print(f"he was {adjective} when it {adverb} went to school")
